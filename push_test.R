4033313af46ebc786782ad594ee97818f366fe31
Angie Bakaj <angiebakaj@Angies-MacBook-Pro.local>
2024-06-07 18:09:52 -0400

    Initial commit

---
+++ push_test.R
@@ -0,0 +1,18 @@
+#' This is a test script
+#' We will run some little functions
+#' Then save this as push_test
+
+
+myvector <- seq(100,1, by = -7)
+
+
+myavg <- mean(myvector)
+myavg
+
+mysum <- sum(myvector)
+mysum
+
+mysd <- sd(myvector)
+mysd
+
+
