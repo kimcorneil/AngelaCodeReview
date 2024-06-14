c1182bafe8924e02d27cc382698d7a1814de80bf
Angie Bakaj <angiebakaj@Angies-MacBook-Pro.local>
2024-06-14 15:31:24 -0400

    created file and attempted user input

---
+++ A2.R
@@ -0,0 +1,13 @@
+#1. Prompt the user to enter a three digit positive number
+#2. Check if the user input is numeric. If not, print an error message and quit.
+#3. Check if the number is narcissistic. A narcissistic number, or an Armstrong number, is a number that is equal to the sum of the cubes of its own digits. 153, 370, 371, 407 are three digit Armstrong numbers.
+#4. Display the result with an appropriate message, e.g. “127 is not an Armstrong number” or “370 is a narcissistic number” or any additional fun text you may want to add.
+
+enter_tdn <- readline(prompt = "Enter a three-digit positive number:")
+
+if (is.na(as.numeric(enter_tdn))) { 
+  cat("Error: Input is not numeric") 
+  quit(save = "no") 
+  }
+
+
