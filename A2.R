a572a5a66884ef0949feaf83fe0b8659a732fdb7
Angie Bakaj <angiebakaj@Angies-MacBook-Pro.local>
2024-06-19 17:33:07 -0400

    -removed assignment questions for reference -added name and title -fixed the error with print (must add "paste" to paste the tdn)

--- A2.R
+++ A2.R
@@ -1,8 +1,5 @@
-#1. Prompt the user to enter a three digit positive number
-#2. Check if the user input is numeric. If not, print an error message and quit.
-#3. Check if the number is narcissistic. A narcissistic number, or an Armstrong number, is a number that is equal to the sum of the cubes of its own digits. 153, 370, 371, 407 are three digit Armstrong numbers.
-#4. Display the result with an appropriate message, e.g. “127 is not an Armstrong number” or “370 is a narcissistic number” or any additional fun text you may want to add.
-
+#ANGELA BAKAJ
+#BTC1855 ASSIGNMENT 2
 
 #1. Prompt the user to enter a three digit positive number
 tdn <- readline(prompt = "Enter a three-digit positive number: ")
@@ -38,9 +35,9 @@ tdn_sum_of_cubes <- sum(tdn_digits^3)
 
 #4. Tdn Narcissism-Check result: In order to check and print if the number is indeed narcissistic, we need to set a condition statement based on the fact that the sum of the tdn's digit's cubes must equal the tdn itself to be considered a narcissistic number.
 if (tdn_sum_of_cubes == tdn) {
-  print(tdn, "is a narcissistic number.")
+  print(paste(tdn, "is a narcissistic number."))
 } else {
-  print(tdn, "is not a narcissistic number." )
+  print(paste(tdn, "is not a narcissistic number." ))
 }
   
 
