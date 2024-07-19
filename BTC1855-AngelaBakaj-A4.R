82b87de00ae5cb3e25a4348c4e9b64c61119a237
Angie Bakaj <angiebakaj@Angies-MacBook-Pro.local>
2024-07-18 20:42:00 -0400

    planning

---
+++ BTC1855-AngelaBakaj-A4.R
@@ -0,0 +1,21 @@
+
+Read the data into a dataframe
+
+Check and correct structural issues
+
+Identify the possible data (content) issues
+
+Your downstream analysis will require the variables 'country', 'shape' and 'duration seconds'.
+Analyze these columns, identify any problems (e.g. missingness, outliers, inconsistency etc) and implement your solution (e.g. remove, impute etc).
+
+NUFORC officials comment on sightings that may be hoax. 
+Identify the best way to identify and remove these sightings from the dataset.
+
+Add another column to the dataset (report_delay) and populate with the time difference in days,
+between the date of the sighting and the date it was reported.
+
+Remove the rows where the sighting was reported before it happened.
+
+Create a table with the average report_delay per country.
+
+Create a histogram using the 'duration seconds' column.
