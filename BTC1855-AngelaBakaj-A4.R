e07224177be42c0cc68aeae87ecb9e54f3b7bbe8
Angie Bakaj <angiebakaj@Angies-MacBook-Pro.local>
2024-07-18 22:21:09 -0400

    Nearly done dealing with the three downstream columns

--- BTC1855-AngelaBakaj-A4.R
+++ BTC1855-AngelaBakaj-A4.R
@@ -1,21 +1,46 @@
+### BTC1855 ASSIGNMENT 4
+### ANGELA BAKAJ
 
-Read the data into a dataframe
+library(dplyr)
+library(tidyr)
 
-Check and correct structural issues
+# Reading in the UFO sightings dataset 
 
-Identify the possible data (content) issues
+ufo_dataset <- read.csv("~/Downloads/ufo_subset.csv", header = TRUE)
+View(ufo_dataset)
+head(ufo_dataset, 10)
 
-Your downstream analysis will require the variables 'country', 'shape' and 'duration seconds'.
-Analyze these columns, identify any problems (e.g. missingness, outliers, inconsistency etc) and implement your solution (e.g. remove, impute etc).
+# Seeing that there are missing values, I want to assign these as NAs
+ufo_dataset[ufo_dataset == ""] <- NA
 
-NUFORC officials comment on sightings that may be hoax. 
-Identify the best way to identify and remove these sightings from the dataset.
+# Checking "Country" 
+# The missing values were already accounted for in the previous step.
+# I want to now check if there are any illegitimate entries for the countries.
+unique(ufo_dataset$country)
+# Using the "unique" function, I see that there are valid country entries: US, GB, CA, AU, DE.
 
-Add another column to the dataset (report_delay) and populate with the time difference in days,
-between the date of the sighting and the date it was reported.
+# Checking "Shape"
+# I will check for any illegitimate entries in the shape column as well.
+unique(ufo_dataset$shape)
+# Using the "unique" function again, I do not identify any invalid shape entries, other than the missing values I already accounted for. 
+# These missing values could be the result of failure to record known shapes OR that the shapes of these sighting were truly unknown.
 
-Remove the rows where the sighting was reported before it happened.
+# Checking "Duration Seconds"
+# First, I'll check if there were any NAs in the "duration.seconds" column to begin with.
+which(is.na(ufo_dataset$duration.seconds))
+# Using the "range" function, I see that there exists extremely small and large values.
+range(ufo_dataset$duration.seconds)
+# Logically speaking, a UFO sighting should not last longer than a few minutes, on average (according to Google)
+# For good measure, I will extend this window of time to say, 8 hours; any duration longer than this is likely an inconsistent result.
 
-Create a table with the average report_delay per country.
 
-Create a histogram using the 'duration seconds' column.
+
+
+# Removing the Hoax sightings:
+# 
+
+
+
+
+
+
